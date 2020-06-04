bc67624a2b7c91cda2f8207b5f3b8b00c70e9049
jszvitor <jszvitor@gmail.com>
2020-06-04 18:30:53 -0300

    feat(day3): added collect items selection

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -34,20 +34,60 @@ function getCities(event) {
 
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
 
   fetch(url)
     .then( res => res.json() )
     .then( cities => {
+      
       for( const city of cities ) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
 
       citySelect.disabled = false
     })
 }
 
+const collectedItems = document.querySelector("input[name=items]")
 
+let selectedItems = [] // stores selected items ID
+
+function handleSelectedItem(event) {
+  const itemLi = event.target // HTML li ELEMENT
+  const itemId = itemLi.dataset.id
+  
+  // add or remove a class from li element with JS
+  itemLi.classList.toggle("selected")
+
+  /* WORK WITH DATA THAT IS SEND FOR API */
+  // verify if there is selected items (se está no array),
+  // if true return the index of selected item
+  const alreadySelected = selectedItems.findIndex( item => item == itemId )
+
+  // if already selected, remove of selection
+  if( alreadySelected != -1 ) {
+    // remove from array with filter
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+    selectedItems = filteredItems
+  } else {
+    // if not already selected, add to selection
+    selectedItems.push(itemId)
+  }
+  
+  // update the hidden input with the selected items
+  collectedItems.value = selectedItems
+}
 
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities )
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect ) {
+  item.addEventListener("click", handleSelectedItem)
+}

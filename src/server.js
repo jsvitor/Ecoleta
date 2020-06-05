6d7cc8b4a26244c2966dd954a04a6ffd9c316b83
jszvitor <jszvitor@gmail.com>
2020-06-05 15:07:12 -0300

    feat(day4): added nodemon.
    `npm install nodemon -D`
    The Nodemon is used to restart the server after any changes. Furthermore, it is a useful development dependency.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const server = express()
 
+
+
+// ways configuration
+// HOME
+server.get("/", (request, response) => {
+  response.send("Cheguei ak")
+})
+
+
 // turn on the server
 server.listen(3000)

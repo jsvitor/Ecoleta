62b32487284c19bee8153973dd20f2e520f71c80
jszvitor <jszvitor@gmail.com>
2020-06-05 15:31:28 -0300

    feat(day4): routes configuration.
    Using express framework:
    - [x] First, I turned visible the public folder;
    - [x] Second, I created routes for the respective views.

--- src/server.js
+++ src/server.js
@@ -3,12 +3,22 @@ const server = express()
 
 
 
+// public dir configuration
+server.use(express.static("public"))
+
 // ways configuration
 // HOME
 server.get("/", (request, response) => {
-  response.send("Cheguei ak")
+  response.sendFile(__dirname + "/views/index.html")
 })
-
+// CREATE POINT
+server.get("/create-point", (request, response) => {
+  response.sendFile(__dirname + "/views/create-point.html")
+})
+// SEARCH RESULTS
+server.get("/search-results", (request, response) => {
+  response.sendFile(__dirname + "/views/search-results.html")
+})
 
 // turn on the server
 server.listen(3000)

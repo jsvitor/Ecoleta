03e9fdfeaebb0ebd29c9fea19ebf1c436b1dd05f
jszvitor <jszvitor@gmail.com>
2020-06-06 20:57:15 -0300

    feat(day5): inserting form data into the database.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const db = require("./database/db")
 // public folder configuration
 server.use(express.static("public"))
 
-
+// turn able the use of request.body in the application
+server.use(express.urlencoded({ extended: true  }))
 
 // using template engine
 const nunjucks = require('nunjucks')
@@ -23,8 +24,55 @@ server.get("/", (request, response) => {
 })
 // CREATE POINT
 server.get("/create-point", (request, response) => {
+  // request.query: Query Strings da nossa url
+  // console.log(request.query)
+
   return response.render("create-point.html")
 })
+// POST
+server.post("/savepoint", (request, response) => {
+  
+  // request.body: O corpo do nosso formulário
+  // 2 Insert data in table
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+
+  const values = [
+    request.body.image,
+    request.body.name,
+    request.body.address,
+    request.body.address2,
+    request.body.state,
+    request.body.city,
+    request.body.items
+  ]
+
+  function afterInsertData(err) {
+    if(err) {
+      console.log(err)
+      return response.send("Erro no cadastro.")
+    }
+
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+    return response.render("create-point.html", { saved: true  })
+  }
+
+  db.run(query, values, afterInsertData) // function passed by reference.
+
+
+})
+
+
 // SEARCH RESULTS
 server.get("/search-results", (request, response) => {
   // take the date from db
